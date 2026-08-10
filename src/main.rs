da77ec27fcdfcce4cf17a4322501fda57d793c6a
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2026-08-10 05:12:16 +0100

    Basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 mod parser;
 use parser::{Question, parser};
-use std::process::{exit, Command};
+use std::{process::Command, io};
 
 fn main() {
+    Command::new("clear");
+    println!("Welcome To QUIZZ - www.github.com/castlesp5");
     let mut vector: Vec<Question> = vec![];
     parser(&mut vector);
     // for elem in vector {
     //     println!("{} == {}", elem.question, elem.answer);
     // }
-    loop {
-        Command::new("clear");
-        exit(0);
+    let mut i = 0;
+    while i < vector.len(){
+        let mut buffer = String::new();
+        println!("Question : {}", vector[i].question);
+        io::stdin().read_line(&mut buffer).expect("FAILED TO READ BUFFER");
+        if buffer.trim() == vector[i].answer.trim() {
+            println!("CORRECT!!!!!! +10points");
+            i += 1;
+        }
+        else {
+            println!("INCORRECT!!! - correct answer is {}", vector[i].answer);
+        }
     }
-    println!("SUCCESS");
+    // println!("SUCCESS");
 }
