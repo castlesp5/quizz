b0718cf715e8d4d6093e193bd13050e0340cd85e
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2026-08-10 04:54:35 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod parser;
-use parser::Question;
-use parser::parser;
+use parser::{Question, parser};
+use std::process::{exit, Command};
 
 fn main() {
     let mut vector: Vec<Question> = vec![];
     parser(&mut vector);
-    for elem in vector {
-        println!("{} == {}", elem.question, elem.answer);
+    // for elem in vector {
+    //     println!("{} == {}", elem.question, elem.answer);
+    // }
+    loop {
+        Command::new("clear");
+        exit(0);
     }
     println!("SUCCESS");
 }
